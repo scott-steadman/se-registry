83facb1b38fe0e52ed9cd19988546c281d50741f
Scott Steadman <ss@stdmn.com>
2020-02-21 12:10:28 -0800

    externalize SMTP host
      I realized that this should probably be externalized
      so it doesn't expose a vulnerability.

--- ruby/rails/config/environments/production.rb
+++ ruby/rails/config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.smtp_settings = {
-                                        :address              => 'middle-earth',
+                                        :address              => ENV['SMTP_HOST'],
                                         :enable_starttls_auto => false,
                                        }
 
