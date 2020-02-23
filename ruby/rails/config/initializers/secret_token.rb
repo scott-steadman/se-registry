d2c645d42e531c72f819e96a63085b75fdf575dc
Scott Steadman <ss@stdmn.com>
2020-02-22 19:55:37 -0800

    upgrade to Rails 5.2
      The tests run, but don't all pass.
    
      Issue #12

--- ruby/rails/config/initializers/secret_token.rb
+++ ruby/rails/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Registry::Application.config.secret_token = ENV['SECRET_TOKEN'] || '0085940ca99e266b89dbb0854e2a8dc8fda0c6246b6f024a6bbc71a190b62fc8bf5c6655fdf9332c91b8626d17c7a41c151f46755fff7f149cdd3be384b5751e'
+Registry::Application.config.secret_key_base = ENV['SECRET_TOKEN'] || '0085940ca99e266b89dbb0854e2a8dc8fda0c6246b6f024a6bbc71a190b62fc8bf5c6655fdf9332c91b8626d17c7a41c151f46755fff7f149cdd3be384b5751e'
