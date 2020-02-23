74b916d501bee8023bdc65b5f5cfdaff3dc2cc93
Scott Steadman <ss@stdmn.com>
2020-02-22 16:19:41 -0800

    fixup production deployment
      I was having trouble getting it to deploy correctly.

--- ruby/rails/config/deploy/production.rb
+++ ruby/rails/config/deploy/production.rb
@@ -1,17 +1,12 @@
 require 'capistrano/deploy_into_docker'
 
-server docker: {container: 'gifts-ruby-2.5'}, user: '99', roles: %w[app]
+server docker: {container: 'gifts-ruby-2.5'}, user: '99:1000', roles: %w[app db web]
 
 set :default_env,     {
                         rails_env: "production",
                       }
 set :deploy_to,       '/app/gifts'
-set :migration_role,  :app
 set :rails_env,       'production'
 set :sshkit_backend,  SSHKit::Backend::Docker
 
-namespace :deploy do
-
-  after :migrate, :'assets:precompile'
-
-end
+append :linked_dirs,  'log', 'tmp'
