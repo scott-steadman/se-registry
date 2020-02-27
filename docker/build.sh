be4549fecedd8394d23144bf3bbb395f77fe6a75
Scott Steadman <ss@stdmn.com>
2020-02-26 18:05:31 -0800

    use tags on container names
      I think it's better indicate the ruby version
      as a tag rather than incorporate it into the
      container name.

--- docker/build.sh
+++ docker/build.sh
@@ -3,6 +3,6 @@
 # turn on debugging
 set -x
 
-docker rmi ss-ruby-2.5
+docker rmi ss:ruby-2.5
 
-docker build -t ss-ruby-2.5 -f Dockerfile .   | tee log/build.log 2>&1
+docker build -t ss:ruby-2.5 -f Dockerfile .   | tee log/build.log 2>&1
--- docker/start.sh
+++ docker/start.sh
@@ -9,10 +9,10 @@ docker run -itd \
 	--restart=unless-stopped \
   --health-cmd='wget -q -O /dev/null  http://localhost:8000/ || exit 1' \
   --env-file .env \
-  --workdir /app/gifts \
+  --workdir /app/gifts/current \
   -u "99:1000" \
 	-v "/srv/gifts.stdmn.com:/app/gifts" \
   --name gifts-ruby-2.5 \
-  ss-ruby-2.5 \
-  current/bin/rails server --pid=/tmp/gifts.pid --binding=0.0.0.0 --port=8000
+  ss:ruby-2.5 \
+  bin/rails server --pid=/tmp/gifts.pid --binding=0.0.0.0 --port=8000
 #  irb
