66ef5c9e80ad5c4605249b988058a403aafd6652
Scott Steadman <ss@stdmn.com>
2021-04-02 15:27:58 -0700

    Issue #22 - prevent error on assets:precompile
      assets:precompile runs in the development environment even
      though the gems were built for the production environment.
    
      So I added a guard that checks for the Listen class (from the listen gem).

--- ruby/rails/config/environments/development.rb
+++ ruby/rails/config/environments/development.rb
@@ -58,7 +58,9 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  if defined?(Listen)
+    config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  end
 
   config.hosts << ENV['HOSTNAME'].split('.').first
 end
