f1ee68ab55baeb202496539099421ef4ffe15087
Scott Steadman <ss@stdmn.com>
2021-04-02 19:46:53 -0700

    update docker-deploy script
      It looks like this is how you re-deploy a container.

--- ruby/rails/bin/docker-deploy.sh
+++ ruby/rails/bin/docker-deploy.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 # turn on debugging
-set -x
+#set -x
 
 DOCKER_TAG=se-registry
 DOCKER_NAME=rails
 #DOCKER_ARGS="--net=host --restart=unless-stopped"
 DOCKER_ARGS="--restart=unless-stopped"
+
+docker stop ${DOCKER_NAME}
+docker rm   ${DOCKER_NAME}
 
 docker run ${DOCKER_ARGS} -itd --name ${DOCKER_NAME} ${DOCKER_TAG}
 
