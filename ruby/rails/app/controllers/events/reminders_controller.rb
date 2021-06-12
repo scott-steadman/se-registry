c2751a7f75f6d15dfc3761af585d38677c1c469a
Scott Steadman <ss@stdmn.com>
2021-06-12 13:05:38 -0700

    refactor RemindersController up to EventsController
      This is the contination of the work started in the previous
      commit.

--- ruby/rails/app/controllers/events/reminders_controller.rb
+++ ruby/rails/app/controllers/events/reminders_controller.rb
@@ -1,80 +1,21 @@
-class Events::RemindersController < ApplicationController
-
-  before_action :require_user
-
-  # GET /reminders
-  # GET /reminders.xml
-  def index
-    @reminders = reminders.paginate(:page => page, :per_page => per_page)
-  end
-
-  # GET /reminders/1
-  # GET /reminders/1.xml
-  def show
-    @reminder = reminders.find(params[:id])
-  end
-
-  # GET /reminders/new
-  # GET /reminders/new.xml
-  def new
-    @reminder = reminders.new
-  end
-
-  # GET /reminders/1/edit
-  def edit
-    @reminder = reminders.find(params[:id])
-  end
-
-  # POST /reminders
-  # POST /reminders.xml
-  def create
-    redirect_to user_reminders_path(page_user) and return unless request.post?
-
-    @reminder = reminders.create!(reminder_params)
-
-    flash[:notice] = 'Event was successfully created.'
-    redirect_to user_reminders_path(page_user)
-
-  rescue StandardError => ex
-    @reminder = reminders.new
-    @reminder.errors.add(:base, ex.message)
-    render :action => :new
-  end
-
-  # PATCH /reminders/1
-  # PATCH /reminders/1.xml
-  def update
-    redirect_to user_reminders_path(page_user) and return unless request.patch?
-
-    @reminder = reminders.find(params[:id])
-
-    if @reminder.update(reminder_params)
-      flash[:notice] = 'Event was successfully updated.'
-      redirect_to user_reminders_path(page_user)
-    else
-      render :action => 'edit'
-    end
-  end
-
-  # DELETE /reminders/1
-  # DELETE /reminders/1.xml
-  def destroy
-    redirect_to user_reminders_path(page_user) and return unless request.delete?
-
-    @reminder = reminders.find(params[:id])
-    @reminder.destroy
-
-    redirect_to user_reminders_path(page_user)
-  end
+class Events::RemindersController < EventsController
 
 private
 
-  def reminder_params
-    params.require(:reminder).permit(:description, :date, :recur)
+  def event_params
+    params[:reminder]&.permit(:description, :date, :recur) || super
   end
 
-  def reminders
+  def events
     page_user.reminders
   end
 
-end
+  def index_path
+    user_reminders_path(page_user)
+  end
+
+  def delete_path(event)
+    user_reminder_path(page_user, event)
+  end
+
+end # class Events::RemindersController
--- ruby/rails/test/controllers/events/reminders_controller_test.rb
+++ ruby/rails/test/controllers/events/reminders_controller_test.rb
@@ -28,38 +28,6 @@ class Events::RemindersControllerTest < ActionController::TestCase
     end
   end
 
-  test 'show requires login' do
-    logout
-    get :show, :params => {:id => @reminder}
-    assert_redirected_to login_path
-  end
-
-  test 'show' do
-    login_as user
-    get :show, :params => {:id => @reminder}
-    assert_response :success
-
-    assert_select "a[href='#{edit_reminder_path(@reminder)}']", "Edit", 'edit link should be rendered'
-  end
-
-  test 'new requires login' do
-    logout
-    get :new
-    assert_redirected_to login_path
-  end
-
-  test 'new' do
-    login_as user
-    get :new
-    assert_response :success
-  end
-
-  test 'create requires login' do
-    logout
-    get :create
-    assert_redirected_to login_path
-  end
-
   test 'create fails on GET' do
     assert_no_difference 'Event.count' do
       login_as user
@@ -68,42 +36,12 @@ class Events::RemindersControllerTest < ActionController::TestCase
     end
   end
 
-  test 'create' do
+  test 'create takes reminder parameter' do
     assert_difference 'Event.count', 1 do
       login_as user
       post :create, :params => {:reminder => {:date => Date.today, :description => 'Today'}}
       assert_redirected_to user_reminders_path(user)
     end
-  end
-
-  test 'create fails' do
-    assert_no_difference 'Reminder.count' do
-      login_as user
-      post :create
-      assert_response :success
-
-      assert_select "div[id=error_explanation]", /value is empty/, 'error message should be rendered'
-    end
-  end
-
-  test 'edit requires login' do
-    logout
-    get :edit, :params => {:id => 42}
-    assert_redirected_to login_path
-  end
-
-  test 'edit' do
-    login_as user
-    get :edit, :params => {:id => @reminder}
-    assert_response :success
-
-    assert_select "form[action='#{user_reminder_path(user, @reminder)}'][method=post]", 1, "edit form should be rendered"
-  end
-
-  test 'update requires login' do
-    logout
-    get :update, :params => {:id => 42}
-    assert_redirected_to login_path
   end
 
   test 'update fails on GET' do
@@ -114,28 +52,6 @@ class Events::RemindersControllerTest < ActionController::TestCase
     assert_not_equal 'foo', @reminder.reload.description, 'reminder should NOT be updated'
   end
 
-  test 'update' do
-    login_as user
-    patch :update, :params => {:id => @reminder, :reminder => {:description => 'foo'}}
-    assert_redirected_to user_reminders_path(user)
-
-    assert_equal 'foo', @reminder.reload.description, 'reminder should be updated'
-  end
-
-  test 'update fails' do
-    login_as user
-    patch :update, :params => {:id => @reminder, :reminder => {:description => nil}}
-    assert_response :success
-
-    assert_select 'div[id=error_explanation]', /Description can't be blank/, 'error message should be rendered'
-  end
-
-  test 'destroy requires login' do
-    logout
-    get :destroy, :params => {:id => 42}
-    assert_redirected_to login_path
-  end
-
   test 'destroy fails on GET' do
     assert_no_difference 'Event.count' do
       login_as user
@@ -144,12 +60,4 @@ class Events::RemindersControllerTest < ActionController::TestCase
     end
   end
 
-  test 'destroy' do
-    assert_difference 'Event.count', -1 do
-      login_as user
-      delete :destroy, :params => {:id => @reminder}
-      assert_redirected_to user_reminders_path(user)
-    end
-  end
-
-end
+end # class Events::RemindersControllerTest
