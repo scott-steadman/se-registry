4e380b3e7974a6dec0a0dda952edf8eb8eb2921b
Scott Steadman <ss@stdmn.com>
2021-07-04 11:34:35 -0700

    Issue #11 - extract error-dialog.js

---
+++ ruby/rails/app/assets/javascripts/v2/error-dialog.js
@@ -0,0 +1,24 @@
+MicroModal.init();
+
+window.showErrorDialog = function(title, message) {
+  var titleElement = document.getElementById('site-modal__title');
+  titleElement.innerText = title;
+
+  var contentElement = document.getElementById('site-modal__content');
+  contentElement.classList.add('is-error');
+  contentElement.innerText = message;
+
+  MicroModal.show('site-modal', {
+    // prevent click from passing through to button/link beneath dialog
+    onClose: function(modal, element, event) {event.preventDefault()},
+  });
+}
+
+window.onAjaxSuccess = function(event, payload, result, request) {
+  var target = this.getAttribute('data-replace-target');
+  if(target) {
+    $('#'+target).replaceWith(payload);
+  }
+}
+
+$(document).on('ajax:success', 'form[data-replace-target]', window.onAjaxSuccess)
