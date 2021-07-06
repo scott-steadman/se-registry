e9a318534c92848288d6836c5a682f081bbcd803
Scott Steadman <ss@stdmn.com>
2021-07-05 17:08:07 -0700

    clean up Friendship model
      Spaces around => look better to me now.

--- ruby/rails/app/models/friendship.rb
+++ ruby/rails/app/models/friendship.rb
@@ -10,8 +10,8 @@ class Friendship < ApplicationRecord
 
   self.table_name = 'friends'
 
-  belongs_to :user,   :class_name=>'User', :foreign_key=>'user_id'
-  belongs_to :friend, :class_name=>'User', :foreign_key=>'friend_id'
+  belongs_to :user,   :class_name => 'User', :foreign_key => 'user_id'
+  belongs_to :friend, :class_name => 'User', :foreign_key => 'friend_id'
 
   attr_accessor :login_or_email
 
