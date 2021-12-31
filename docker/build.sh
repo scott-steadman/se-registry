a1bb6eac6fdb58837451564fb9e9998f2875470b
Scott Steadman <ss@stdmn.com>
2021-12-31 07:25:18 -0800

    remove docker subdir
      This functionality was moved to ruby/rails/bin/docker-*.sh

--- docker/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# turn on debugging
-set -x
-
-docker rmi ss:ruby-2.5
-
-docker build -t ss:ruby-2.5 --pull --no-cache -f Dockerfile .   | tee log/build.log 2>&1
--- docker/stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# enable debugging
-set -x
-
-docker kill gifts-ruby-2.5
-docker rm gifts-ruby-2.5
