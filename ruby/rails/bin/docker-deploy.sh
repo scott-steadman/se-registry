bf9eea46e4800c04d8f3825a0b18cf452ca3d519
Scott Steadman <ss@stdmn.com>
2022-11-04 14:08:14 -0700

    add image pruning to docker-deploy.sh
      I like to keep things tidy.

--- ruby/rails/bin/docker-deploy.sh
+++ ruby/rails/bin/docker-deploy.sh
@@ -8,11 +8,14 @@ DOCKER_NAME=prod-${PROJECT}
 
 docker stop ${DOCKER_NAME}
 docker rm   ${DOCKER_NAME}
-docker image prune --all
 
 docker run \
   --restart unless-stopped \
   --name ${DOCKER_NAME} \
   -itd ${PROJECT}:latest
 
+docker image prune --all
+
 docker logs -f ${DOCKER_NAME} 
+
+
