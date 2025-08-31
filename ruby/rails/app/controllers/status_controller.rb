9286b557c10bb05dbb8859603a45f52fbd95f289
Scott Steadman <ss@stdmn.com>
2025-08-31 07:15:09 -0700

    add rails_env to status/index

--- ruby/rails/app/controllers/status_controller.rb
+++ ruby/rails/app/controllers/status_controller.rb
@@ -3,6 +3,7 @@ class StatusController < ApplicationController
   def index
     result = {
       :db_result     => ActiveRecord::Base.connection.execute("SELECT version()").column_values(0).first,
+      :rails_env     => Rails.env,
       :rails_version => Rails.version,
       :ruby_version  => RUBY_VERSION,
       :status        => 'OK',
