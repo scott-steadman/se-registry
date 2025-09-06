61f1dde72a6f5b7126560b5528d212d731a8d9ac
Scott Steadman <ss@stdmn.com>
2025-09-06 14:39:48 -0700

    add RAILS_GROUPS_OVERRIDE to docker-run
      Otherwise the app will blow up trying to load gems from the
      development and test groups.

--- ruby/rails/bin/docker-run.sh
+++ ruby/rails/bin/docker-run.sh
@@ -9,6 +9,7 @@ RAILS_ENV=${1:-development}
 docker run \
   --env HOSTNAME=${HOSTNAME} \
   --env RAILS_ENV=${RAILS_ENV} \
+  --env RAILS_GROUPS_OVERRIDE="default,production" \
   --env RAILS_MASTER_KEY=$(cat config/credentials/${RAILS_ENV}.key) \
   --publish 3000:3000 \
   -it ${PROJECT}:latest ./bin/rails server -b 0.0.0.0
