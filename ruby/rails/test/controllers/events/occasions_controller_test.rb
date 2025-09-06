56893afa5c7f806cd2ea33f94267f285586c21f5
Scott Steadman <ss@stdmn.com>
2025-09-06 11:56:04 -0700

    fix v2/occaisons bug
      Looks like we need to pass @events to the will_paginate call in
      index.html.erb.

--- ruby/rails/test/controllers/events/occasions_controller_test.rb
+++ ruby/rails/test/controllers/events/occasions_controller_test.rb
@@ -2,29 +2,31 @@ require 'test_helper'
 
 class Events::OccasionsControllerTest < ActionController::TestCase
 
-  def setup
-    @occasion = create_occasion(:user => user, :description => 'Occasion Description')
-    @reminder = create_reminder(:user => user, :description => 'Reminder Description')
-    @event    = create_event(:user => user, :description => 'Event Description')
-  end
-
   test 'index requires login' do
     logout
     get :index
     assert_redirected_to login_path
   end
 
+  test 'index v2 with no occasions' do
+    login_as user(:ui_version => 2)
+    get :index
+    assert_response :success
+  end
+
   test 'index with no path only shows occasions' do
+    [occasion, event, reminder] # create events
+
     login_as user
     get :index
     assert_response :success
 
-    [@occasion].each do |event|
-      assert_select "td", event.description, 'event should be rendered'
+    [occasion].each do |ii|
+      assert_select "td", ii.description, 'event should be rendered'
     end
 
-    [@event, @reminder].each do |event|
-      assert_select "td", {:count => 0, :text => event.description}, 'event should NOT be rendered'
+    [event, reminder].each do |ii|
+      assert_select "td", {:count => 0, :text => ii.description}, 'event should NOT be rendered'
     end
   end
 
@@ -46,18 +48,35 @@ class Events::OccasionsControllerTest < ActionController::TestCase
 
   test 'update fails on GET' do
     login_as user
-    get :update, :params => {:id => @occasion, :occasion => {:description => 'foo'}}
+    get :update, :params => {:id => occasion, :occasion => {:description => 'foo'}}
     assert_redirected_to user_occasions_path(user)
 
-    assert_not_equal 'foo', @occasion.reload.description, 'occasion should NOT be updated'
+    assert_not_equal 'foo', occasion.reload.description, 'occasion should NOT be updated'
   end
 
   test 'destroy fails on GET' do
+    occasion # create occasion
+
     assert_no_difference 'Event.count' do
       login_as user
-      get :destroy, :params => {:id => @occasion}
+      get :destroy, :params => {:id => occasion}
       assert_redirected_to user_occasions_path(user)
     end
   end
+
+private
+
+  def occasion
+    @occasion ||= create_occasion(:user => user, :description => 'Occasion Description')
+  end
+
+  def reminder
+    @reminder ||= create_reminder(:user => user, :description => 'Reminder Description')
+  end
+
+  def event
+    @event ||= create_event(:user => user, :description => 'Event Description')
+  end
+
 
 end # class Events::OccasionsControllerTest
